993b48572085f5b685a6f214951fbb980ccdf5c6
MarkHatTHORinc <markh@thorinc.com>
2021-07-19 10:29:34 -0400

    add more js code

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://MarkHatTHORinc.github.io/",
+    "name": "Mark Harrison",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/markhatthorinc/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/markhatthorinc",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"http://twitter.com/markhatthorinc",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a Manager of Engineering for a large Healthcare Software Organization. I am also working on my certification for MERN Full Stack Development.",
+    "address":"Atlanta, Georgia",
+    "website":"https://markhatthorinc.github.io",
+    "education":[
+      {
+        "UniversityName":"Georgia Tech",
+        "specialization":"MERN Full Stack Developer",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2021",
+        "Achievements":"MERN Full Stack Developer Certification"
+      },
+      {
+        "UniversityName":"University of New Mexico",
+        "specialization":"Bachelor of Computer Science",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"1987",
+        "Achievements":"Bachelors Degree"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"WellSky",
+        "specialization":"Engineering Manager",
+        "MonthOfLeaving":"Current",
+        "YearOfLeaving":" ",
+        "Achievements":"Engineering Manager"
+      },
+      {
+        "CompanyName":"Kubota of North America/TPM",
+        "specialization":"Development Manager",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2021",
+        "Achievements":"Sucessfully managed a productive develpment team"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Mark Stout"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Bob Baker"
+      }
+    ]
+  }
+  
+  export default resumeData
